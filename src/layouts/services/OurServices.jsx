d4515f0ba1dd3d4355fc65624574cbad13ca387b
dev-artouises <dev.artlouises@gmail.com>
2024-10-07 05:50:12 +0800

    tbc

--- src/layouts/services/OurServices.jsx
+++ src/layouts/services/OurServices.jsx
@@ -1,75 +1,58 @@
-import { Box, Container, Typography } from "@mui/material"
-import ButtonComponent from "../../components/common/ButtonComponent"
+import { Box, Container, Typography } from "@mui/material";
+import ButtonComponent from "../../components/common/ButtonComponent";
 
-import ImageDiscuss from '../../assets/services.png'
+import ImageDiscuss from "../../assets/services.png";
 
 const Services = () => {
-    return (
-        <Box
-            sx={{
-                paddingTop: '64px',
-                paddingBottom: '64px'
-            }}
-        >
-            <Container>
-                <Box>
-                    <Box
-                        textAlign='center'
-                        marginBottom={4}
-                    >
-                        <Typography
-                            variant="body1"
-                            gutterBottom
-                            component="P"
-                            fontSize='1rem'
-                            fontWeight='500'
-                            textTransform='uppercase'
-                        >
-                            Our Services
-                        </Typography>
+  return (
+    <Box
+      sx={{
+        paddingTop: "64px",
+        paddingBottom: "64px",
+      }}
+    >
+      <Container>
+        <Box>
+          <Box textAlign="center" marginBottom={4}>
+            <Typography
+              variant="body1"
+              gutterBottom
+              component="P"
+              fontSize="1rem"
+              fontWeight="500"
+              textTransform="uppercase"
+            >
+              Our Services
+            </Typography>
 
+            <Typography variant="h3" gutterBottom fontWeight="700">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </Typography>
 
-                        <Typography
-                            variant="h3"
-                            gutterBottom
-                            fontWeight='700'
-                        >
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        </Typography>
+            <Typography variant="h6" component="p">
+              Ipsa labore saepe ducimus eum laudantium, aperiam libero rem
+              minima! Dolor sunt explicabo dicta distinctio molestiae nemo
+              officia aliquam, quibusdam expedita consequuntur!
+            </Typography>
 
-                        <Typography
-                            variant="h6"
-                            component='p'
-                        >
-                            Ipsa labore saepe ducimus eum laudantium, aperiam libero rem minima! Dolor sunt explicabo dicta distinctio molestiae nemo officia aliquam, quibusdam expedita consequuntur!
-                        </Typography>
+            <Box marginTop={3} display="flex" justifyContent="center">
+              <ButtonComponent
+                label="Contact us"
+                variant="contained"
+                size="large"
+              />
+            </Box>
+          </Box>
 
-                        <Box
-                            marginTop={3}
-                            display='flex'
-                            justifyContent='center'
-                        >
-                            <ButtonComponent
-                                label='Contact us'
-                                variant='contained'
-                                size='large'
-                            />
-                        </Box>
-                    </Box>
-
-                    {/* Services Image Section */}
-                    <Box
-                        display='flex'
-                        justifyContent='center'
-                        alignItems='center'
-                    >
-                        <img
-                            src={ImageDiscuss}
-                            alt='services-image'
-                            width={520}
-                            height={520}
-                        />
-                        {/* 
+          {/* Services Image Section */}
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <img
+              src={ImageDiscuss}
+              alt="services-image"
+              width={520}
+              height={520}
+            />
+            {/* 
                                 <Card
                                     sx={{
                                         textAlign: 'center'
@@ -85,12 +68,11 @@ const Services = () => {
                                         }}
                                     />
                                 </Card> */}
-                    </Box>
-                </Box>
-            </Container>
-
+          </Box>
         </Box>
-    )
-}
+      </Container>
+    </Box>
+  );
+};
 
-export default Services
+export default Services;
